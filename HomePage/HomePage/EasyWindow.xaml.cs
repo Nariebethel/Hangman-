e3ae991a9a45f8f6c5413474ec803354a821ccc4
Stephen <jt13smt@gmail.com>
2019-04-05 22:21:22 -0500

    Added list of words

---
+++ HomePage/HomePage/EasyWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hangman
+{
+   /// <summary>
+   /// Interaction logic for EasyWindow.xaml
+   /// </summary>
+   public partial class EasyWindow : Window
+   {
+      List<string> easy_list = new List<string>()
+      {
+         "apex",
+         "dogs",
+         "cats",
+         "deer",
+         "epic",
+         "frog",
+         "glow",
+         "zone",
+         "whig",
+         "lazy"
+      };
+
+      public EasyWindow()
+      {
+         InitializeComponent();
+      }
+   }
+}
